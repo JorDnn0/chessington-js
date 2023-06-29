0b3d7ca844f67a6ead8cff59196073932e34cb9c
JorDnn0 <90001659+JorDnn0@users.noreply.github.com>
2023-06-29 16:34:23 +0100

    rook moves laterally

--- src/engine/pieces/rook.js
+++ src/engine/pieces/rook.js
@@ -1,4 +1,7 @@
 import Piece from './piece';
+import Board from "../board";
+import gameSettings from "../gameSettings";
+import Square from "../square";
 
 export default class Rook extends Piece {
     constructor(player) {
@@ -6,6 +9,17 @@ export default class Rook extends Piece {
     }
 
     getAvailableMoves(board) {
-        return new Array(0);
+        let pos = board.findPiece(this)
+        let moves = []
+
+        for(let i = 0; i < gameSettings.BOARD_SIZE; i++) {
+            //all columns
+            if(i !== pos.col) moves.push(new Square(pos.row,i))
+
+            //all rows
+            if(i !== pos.row) moves.push(new Square(i,pos.col))
+        }
+
+        return moves
     }
 }
