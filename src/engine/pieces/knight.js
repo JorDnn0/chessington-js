555d29d35175b71c21e9e9d095f576c69eaf8ec1
JorDnn0 <90001659+JorDnn0@users.noreply.github.com>
2023-06-29 17:28:22 +0100

    knights (unfinished)

--- src/engine/pieces/knight.js
+++ src/engine/pieces/knight.js
@@ -1,4 +1,6 @@
 import Piece from './piece';
+import gameSettings from "../gameSettings";
+import Square from "../square";
 
 export default class Knight extends Piece {
     constructor(player) {
@@ -6,6 +8,22 @@ export default class Knight extends Piece {
     }
 
     getAvailableMoves(board) {
-        return new Array(0);
+        let pos = board.findPiece(this)
+        let moves = []
+        let boardSize = gameSettings.BOARD_SIZE
+
+        let directions = [
+            [1,2],[1,-2],[-1,2],[-1,-2],
+            [2,1],[2,-1],[-2,1],[-2,-1]
+        ]
+
+        for(let dir in directions) {
+            let newPos = new Square(pos.row,pos.col)
+            if() {
+
+            }
+        }
+
+        return moves
     }
 }
